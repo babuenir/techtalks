83c656e721654e88443776da4e04eb734fd9a677
BabuSubashChandar <babuenir@gmail.com>
2016-10-16 01:41:42 +0530

    Adding step to push talks to blog.

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+TALKS="$(find . -name * -type d)"
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+mkdir output
+cd output
+mkdir talks
+for talk in $TALKS;do
+    cp -rf $talk/output/* talks
+done
+
+git init
+git config user.name "BabuSubashChandar"
+git config user.email "babuenir@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/babuenir/blog.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+#echo "babuenir.github.io" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
